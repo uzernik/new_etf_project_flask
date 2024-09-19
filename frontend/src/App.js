ea0ca7a67a29c6fecf9346aeef83812bf01a1fa9
Uri Zernik <uri.zernik@gmail.com>
2024-09-19 15:16:45 -0700

    Initial commit with Dockerized React and Flask app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        ETF Visualization Project
+      </header>
+    </div>
+  );
+}
+
+export default App;
